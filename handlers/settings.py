35d4192788d15f31e658cab520db2c41a675d26b
Mikhail Utkin <mihut@rambler.ru>
2023-03-22 18:15:29 +0500

    test

--- handlers/settings.py
+++ handlers/settings.py
@@ -19,11 +19,14 @@ class SettingsFSM (StatesGroup):
     ingr_input = State()
     diet_input = State()
     health_input = State()
+    cuisineType_input = State()
+    dishType_input = State()
+    time_input = State()
+    excluded_input = State()
 
 
-# @dp.message_handler(commands=['setings'], state='*')
 async def settings_command(message: types.Message):
-    urs_object = database.get_user_recipe_settings_by_user_id(message.from_user.id)
+    urs_object, session = database.get_user_recipe_settings_by_user_id(message.from_user.id)
     await message.answer(f"Текущие настройки:\n"
                          f"Количество ингридиентов в рецептах (/ingr): {urs_object.ingr}\n"
                          f"Тип диеты (/diet): {urs_object.diet}\n"
@@ -36,46 +39,88 @@ async def settings_command(message: types.Message):
     await SettingsFSM.main_menu.set()
 
 
-# @dp.message_handler(commands=['ingr'], state=SettingsFSM.main_menu)
 async def ingr_command(message: types.Message, state: FSMContext):
     await message.answer("Введите количество ингридиентов\n/cancel - отменить всё")
     await SettingsFSM.ingr_input.set()
 
 
-# @dp.message_handler(state=SettingsFSM.ingr_input)
 async def ingr_input(message: types.Message, state: FSMContext):
-    urs_object = database.get_user_recipe_settings_by_user_id(message.from_user.id)
-    async with state.proxy() as data:
-        urs_object.ingr = message.text
-        urs_object.update_user_recipe_settings()
+    urs_object, session = database.get_user_recipe_settings_by_user_id(message.from_user.id)
+    urs_object.ingr = message.text
+    database.update_user_recipe_settings(session)
     await settings_command(message)
 
 
-# @dp.message_handler(commands=['diet'], state=SettingsFSM.main_menu)
 async def diet_command(message: types.Message, state: FSMContext):
     await message.answer("Введите тип диеты\n/cancel - отменить всё")
     await SettingsFSM.diet_input.set()
 
 
-# @dp.message_handler(state=SettingsFSM.diet_input)
 async def diet_input(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['diet'] = message.text
+    urs_object, session = database.get_user_recipe_settings_by_user_id(message.from_user.id)
+    urs_object.diet = message.text
+    database.update_user_recipe_settings(session)
     await settings_command(message)
 
 
-# @dp.message_handler(commands=['health'], state=SettingsFSM.main_menu)
 async def health_command(message: types.Message, state: FSMContext):
     await message.answer("Введите тип здоровости еды\n/cancel - отменить всё")
     await SettingsFSM.health_input.set()
 
 
-# @dp.message_handler(state=SettingsFSM.health_input)
 async def health_input(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['health'] = message.text
+    urs_object, session = database.get_user_recipe_settings_by_user_id(message.from_user.id)
+    urs_object.health = message.text
+    database.update_user_recipe_settings(session)
     await settings_command(message)
 
+
+async def cuisineType_command(message: types.Message, state: FSMContext):
+    await message.answer("Введите тип здоровости еды\n/cancel - отменить всё")
+    await SettingsFSM.cuisineType_input.set()
+
+
+async def cuisineType_input(message: types.Message, state: FSMContext):
+    urs_object, session = database.get_user_recipe_settings_by_user_id(message.from_user.id)
+    urs_object.cuisineType = message.text
+    database.update_user_recipe_settings(session)
+    await settings_command(message)
+
+
+async def dishType_command(message: types.Message, state: FSMContext):
+    await message.answer("Введите тип здоровости еды\n/cancel - отменить всё")
+    await SettingsFSM.dishType_input.set()
+
+
+async def dishType_input(message: types.Message, state: FSMContext):
+    urs_object, session = database.get_user_recipe_settings_by_user_id(message.from_user.id)
+    urs_object.dishType = message.text
+    database.update_user_recipe_settings(session)
+    await settings_command(message)
+
+
+async def time_command(message: types.Message, state: FSMContext):
+    await message.answer("Введите тип здоровости еды\n/cancel - отменить всё")
+    await SettingsFSM.time_input.set()
+
+
+async def time_input(message: types.Message, state: FSMContext):
+    urs_object, session = database.get_user_recipe_settings_by_user_id(message.from_user.id)
+    urs_object.time = message.text
+    database.update_user_recipe_settings(session)
+    await settings_command(message)
+
+
+async def excluded_command(message: types.Message, state: FSMContext):
+    await message.answer("Введите тип здоровости еды\n/cancel - отменить всё")
+    await SettingsFSM.excluded_input.set()
+
+
+async def excluded_input(message: types.Message, state: FSMContext):
+    urs_object, session = database.get_user_recipe_settings_by_user_id(message.from_user.id)
+    urs_object.excluded = message.text
+    database.update_user_recipe_settings(session)
+    await settings_command(message)
 
 
 def register_handlers(dp: Dispatcher):
@@ -86,3 +131,11 @@ def register_handlers(dp: Dispatcher):
     dp.register_message_handler(diet_input, state=SettingsFSM.diet_input)
     dp.register_message_handler(health_command, commands=['health'], state=SettingsFSM.main_menu)
     dp.register_message_handler(health_input, state=SettingsFSM.health_input)
+    dp.register_message_handler(cuisineType_command, commands=['cuisineType'], state=SettingsFSM.main_menu)
+    dp.register_message_handler(cuisineType_input, state=SettingsFSM.cuisineType_input)
+    dp.register_message_handler(dishType_command, commands=['dishType'], state=SettingsFSM.main_menu)
+    dp.register_message_handler(dishType_input, state=SettingsFSM.dishType_input)
+    dp.register_message_handler(time_command, commands=['time'], state=SettingsFSM.main_menu)
+    dp.register_message_handler(time_input, state=SettingsFSM.time_input)
+    dp.register_message_handler(excluded_command, commands=['excluded'], state=SettingsFSM.main_menu)
+    dp.register_message_handler(excluded_input, state=SettingsFSM.excluded_input)
